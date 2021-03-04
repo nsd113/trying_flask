d900c2169f4feb654cca2165648dc86e2bc20c33
nsd113 <dubatovan@gmail.com>
2021-03-04 15:28:48 +0000

    URL routing. Adding a basic front end to Flask app. Added template inheritance. Linking in CSS style sheet. Linking in Javascript files. Adding database to Flask app (sqlalchemy). Creating Flask models. Hooking up database to front end

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    dateposted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' + str(self.id)
+
+
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'This is the content of post 1.',
+        'author': 'Zilazni'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'This is the content of post 2.'
+    }
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/posts', methods=['GET', 'POST'])
+def posts():
+    return render_template('posts.html', posts=all_posts) 
+
+if __name__ == "__main__":
+    app.run(debug=True)
