506b8b30f560b694eaef1951e4e41594c6331a10
nsd113 <dubatovan@gmail.com>
2021-03-25 19:16:43 +0000

    added delete posts function, and Author for each post

--- app.py
+++ app.py
@@ -18,37 +18,30 @@ class BlogPost(db.Model):
         return 'Blog post ' + str(self.id)
 
 
-all_posts = [ #just to delite it later
-    {
-        'title': 'Post 1',
-        'content': 'This is the content of post 1.',
-        'author': 'Zilazni'
-    },
-    {
-        'title': 'Post 2',
-        'content': 'This is the content of post 2.'
-    }
-]
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/posts', methods=['GET', 'POST'])
-
 def posts():
-
     if request.method == 'POST': #read all data from the form and save it to DB
         post_title = request.form['title']
         post_content = request.form['content']
-        new_post = BlogPost(title=post_title, content=post_content, author='Aaron')
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
         db.session.add(new_post) #adding to db
         db.session.commit()
         return redirect('/posts')
     else: # if we are getting- display existing db 
         all_posts = BlogPost.query.order_by(BlogPost.dateposted).all()
+        return render_template('posts.html', posts=all_posts) 
 
-    return render_template('posts.html', posts=all_posts) 
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id) #if we dont have post #id - it do not break
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
 
 if __name__ == "__main__":
     app.run(debug=True)
